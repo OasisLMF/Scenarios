8a02f24e811518b1baba861659334a106c53815c
Vinul Wimalaweera <16556525+vinulw@users.noreply.github.com>
2025-05-02 17:01:32 +0100

    handle large files jba

---
+++ get_s3_data.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+aws s3 cp s3://oasislmf-scenarios/JBA/Philippines-Taiwan/FLRF/U/keys_data/areaperil_dict.csv ./JBA/Philippines-Taiwan/FLRF/U/keys_data/areaperil_dict.csv
+aws s3 cp s3://oasislmf-scenarios/JBA/Philippines-Taiwan/FLRF/D/keys_data/areaperil_dict.csv ./JBA/Philippines-Taiwan/FLRF/D/keys_data/areaperil_dict.csv
+aws s3 cp s3://oasislmf-scenarios/JBA/Ghana/E635900/FLRF/keys_data/areaperil_dict.csv ./JBA/Ghana/E635900/FLRF/keys_data/areaperil_dict.csv
+aws s3 cp s3://oasislmf-scenarios/JBA/Ghana/E760461/FLRF/keys_data/areaperil_dict.csv ./JBA/Ghana/E760461/FLRF/keys_data/areaperil_dict.csv
+aws s3 cp s3://oasislmf-scenarios/JBA/Nepal/E505423/FLRF/keys_data/areaperil_dict.csv ./JBA/Nepal/E505423/FLRF/keys_data/areaperil_dict.csv
+aws s3 cp s3://oasislmf-scenarios/JBA/Nepal/E151185/FLRF/keys_data/areaperil_dict.csv ./JBA/Nepal/E151185/FLRF/keys_data/areaperil_dict.csv
