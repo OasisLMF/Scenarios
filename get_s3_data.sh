484ed0504ac597d1c16df3e8f3c9f50584962fe3
Vinul Wimalaweera <16556525+vinulw@users.noreply.github.com>
2025-05-02 17:06:38 +0100

    large nepal files

--- get_s3_data.sh
+++ get_s3_data.sh
@@ -5,3 +5,8 @@ aws s3 cp s3://oasislmf-scenarios/JBA/Ghana/E635900/FLRF/keys_data/areaperil_dic
 aws s3 cp s3://oasislmf-scenarios/JBA/Ghana/E760461/FLRF/keys_data/areaperil_dict.csv ./JBA/Ghana/E760461/FLRF/keys_data/areaperil_dict.csv
 aws s3 cp s3://oasislmf-scenarios/JBA/Nepal/E505423/FLRF/keys_data/areaperil_dict.csv ./JBA/Nepal/E505423/FLRF/keys_data/areaperil_dict.csv
 aws s3 cp s3://oasislmf-scenarios/JBA/Nepal/E151185/FLRF/keys_data/areaperil_dict.csv ./JBA/Nepal/E151185/FLRF/keys_data/areaperil_dict.csv
+aws s3 cp s3://oasislmf-scenarios/JBA/Nepal/E505423/FLRF/model_data/footprint.csv ./JBA/Nepal/E505423/FLRF/model_data/footprint.csv
+aws s3 cp s3://oasislmf-scenarios/JBA/Nepal/E505423/FLRF/keys_data/areaperil_dict.csv ./JBA/Nepal/E505423/FLRF/keys_data/areaperil_dict.csv
+aws s3 cp s3://oasislmf-scenarios/JBA/Nepal/E151185/FLRF/model_data/footprint.csv ./JBA/Nepal/E151185/FLRF/model_data/footprint.csv
+aws s3 cp s3://oasislmf-scenarios/JBA/Nepal/E151185/FLRF/keys_data/areaperil_dict.csv ./JBA/Nepal/E151185/FLRF/keys_data/areaperil_dict.csv
+aws s3 cp s3://oasislmf-scenarios/JBA/Nepal/E432557/FLRF/keys_data/areaperil_dict.csv ./JBA/Nepal/E432557/FLRF/keys_data/areaperil_dict.csv
