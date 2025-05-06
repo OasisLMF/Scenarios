80657d496bf1bbde0fb9192cc13b653f3fb8f44a
Vinul Wimalaweera <16556525+vinulw@users.noreply.github.com>
2025-05-06 15:07:14 +0100

    convert jba-ghana areaperil_dict to parquet

---
+++ JBA/s3upload.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# aws s3 sync ./Ghana s3://oasislmf-scenarios/JBA/Ghana
+aws s3 sync ./Nepal s3://oasislmf-scenarios/JBA/Nepal
+aws s3 sync ./Philippines-Taiwan s3://oasislmf-scenarios/JBA/Philippines-Taiwan
