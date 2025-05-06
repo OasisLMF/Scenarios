8e43bdcf5f3427bc91a5478a45029ef9327ff1ac
Vinul Wimalaweera <16556525+vinulw@users.noreply.github.com>
2025-05-06 16:35:27 +0100

    jba-ghana update s3 script areperil_dict

--- get_s3_data.sh
+++ get_s3_data.sh
@@ -14,3 +14,5 @@ aws s3 cp s3://oasislmf-scenarios/JBA/Philippines-Taiwan/FLRF/U/model_data/footp
 aws s3 cp s3://oasislmf-scenarios/JBA/Philippines-Taiwan/FLRF/U/keys_data/areaperil_dict.csv ./JBA/Philippines-Taiwan/FLRF/U/keys_data/areaperil_dict.csv
 aws s3 cp s3://oasislmf-scenarios/JBA/Philippines-Taiwan/FLRF/D/model_data/footprint.csv ./JBA/Philippines-Taiwan/FLRF/D/model_data/footprint.csv
 aws s3 cp s3://oasislmf-scenarios/JBA/Philippines-Taiwan/FLRF/D/keys_data/areaperil_dict.csv ./JBA/Philippines-Taiwan/FLRF/D/keys_data/areaperil_dict.csv
+aws s3 cp s3://oasislmf-scenarios/JBA/Ghana/E635900/FLRF/keys_data/areaperil_dict.parquet ./JBA/Ghana/E635900/FLRF/keys_data/areaperil_dict.parquet
+aws s3 cp s3://oasislmf-scenarios/JBA/Ghana/E760461/FLRF/keys_data/areaperil_dict.parquet ./JBA/Ghana/E760461/FLRF/keys_data/areaperil_dict.parquet
