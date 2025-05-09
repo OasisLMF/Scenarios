5cd7ba9d636c149f170eeab5f534a65a8e00e0e6
Vinul Wimalaweera <16556525+vinulw@users.noreply.github.com>
2025-05-09 11:32:51 +0100

    reduced s3 files

---
+++ get_s3_data_reduced.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+aws s3 cp s3://oasislmf-scenarios/JBA/Ghana/E635900/FLRF/keys_data/areaperil_dict.parquet ./JBA/Ghana/E635900/FLRF/keys_data/areaperil_dict.parquet
+aws s3 cp s3://oasislmf-scenarios/JBA/Ghana/E760461/FLRF/keys_data/areaperil_dict.parquet ./JBA/Ghana/E760461/FLRF/keys_data/areaperil_dict.parquet
+aws s3 cp s3://oasislmf-scenarios/JBA/Ghana/E635900/combined/keys_data/areaperil_dict.parquet ./JBA/Ghana/E635900/combined/keys_data/areaperil_dict.parquet
+aws s3 cp s3://oasislmf-scenarios/JBA/Ghana/E760461/combined/keys_data/areaperil_dict.parquet ./JBA/Ghana/E760461/combined/keys_data/areaperil_dict.parquet
+aws s3 cp s3://oasislmf-scenarios/Nepal/E505423/combined/keys_data/areaperil_dict.parquet ./Nepal/E505423/combined/keys_data/areaperil_dict.parquet
+aws s3 cp s3://oasislmf-scenarios/Nepal/E151185/combined/keys_data/areaperil_dict.parquet ./Nepal/E151185/combined/keys_data/areaperil_dict.parquet
+aws s3 cp s3://oasislmf-scenarios/JBA/Philippines-Taiwan/combined-undefended/keys_data/areaperil_dict.parquet ./JBA/Philippines-Taiwan/combined-undefended/keys_data/areaperil_dict.parquet
