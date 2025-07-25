a6f2b2b41ac31629e6337740bbd98cd1875ad708
Vinul Wimalaweera <16556525+vinulw@users.noreply.github.com>
2025-07-25 17:03:25 +0100

    improve file upload + sync

--- s3upload.sh
+++ s3upload.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-aws s3 sync ./JBA/Ghana s3://oasislmf-scenarios/JBA/Ghana
-aws s3 sync ./JBA/Nepal s3://oasislmf-scenarios/JBA/Nepal
-aws s3 sync ./JBA/Philippines-Taiwan s3://oasislmf-scenarios/JBA/Philippines-Taiwan
+xargs -a models.txt -I {} aws s3 sync ./{} s3://oasislmf-scenarios/{} \
+  --exclude '*runs/*'
