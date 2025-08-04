240c9abc025631d5244cbc1495a48e6560fd0212
Vinul Wimalaweera <16556525+vinulw@users.noreply.github.com>
2025-08-04 16:33:29 +0100

    sync instead of copy

--- get_s3_data.sh
+++ get_s3_data.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-xargs -a large_files.txt -I {} aws s3 cp s3://oasislmf-scenarios/{} ./{}
+xargs -a large_files.txt -I {} aws s3 sync s3://oasislmf-scenarios/{} ./{}
--- get_s3_data_reduced.sh
+++ get_s3_data_reduced.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-xargs -a large_files_reduced.txt -I {} aws s3 cp s3://oasislmf-scenarios/{} ./{}
+xargs -a large_files_reduced.txt -I {} aws s3 sync s3://oasislmf-scenarios/{} ./{}
