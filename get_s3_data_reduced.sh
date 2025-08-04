e32a7f4340a0b448d45e4062dc6cce8a53f7a37b
Vinul Wimalaweera <16556525+vinulw@users.noreply.github.com>
2025-08-04 17:06:43 +0100

    WIP - s3 no sign in

--- get_s3_data_reduced.sh
+++ get_s3_data_reduced.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-xargs -a large_files_reduced.txt -I {} aws s3 --no-sign-request sync s3://oasislmf-scenarios/{} ./{}
+xargs -a large_files_reduced.txt -I {} aws s3 --no-sign-request cp s3://oasislmf-scenarios/{} ./{}
